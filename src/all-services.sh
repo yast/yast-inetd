99091b73f3bd58472bccf85358838cb49b1dc76d
Martin Vidner <mvidner@suse.cz>
2003-01-15 09:09:52 +0000

    Updated to include the program name (to detect installed services).
    svn path=/trunk/inetd/; revision=7725

--- src/all-services.sh
+++ src/all-services.sh
@@ -5,8 +5,10 @@
 # Find all files in /etc/xinetd.d in the autobuild
 # and create a ycp file linking the declared services and the packages
 # Currently the format is:
-# A list of maps, having the keys "package", "service" and "port"
+# A list of maps, having the keys "package", "service", "port", "program"
 # and string values
+# Program is the base name. It is the real program, not tcpd.
+
 SX=all-services
 :> $SX.ycp
 exec > $SX.ycp
@@ -20,19 +22,19 @@ echo -n "// Generated on "
 LANG=C date
 echo "["
 sort $SX-pkgs | while read pkg; do
-    rm -f $DIR/*
+    mv -f $DIR/* $DIR.bak
     rpm2cpio /work/CDs/all/full-i386/suse/*/$pkg.rpm \
 	| cpio -idvm --no-absolute-filenames "$DIR/*"
     # Use the agent to parse the config files
-    # It has a hack to print "$service\t$port" to stderr
+    # It has a hack to print "$service,$port,$program" to fd3
     {
 	echo "Xinetd(\"./$SX.conf\")"
 	# services-by-package
 	echo "Execute(.sbp)"
 	echo "result(nil)"
-    } | ../agents/ag_netd 2>&1 >/dev/null \
-	| sed "s/^/$pkg	&/" \
-	| awk '{print "$[\"package\":\"" $1 "\", \"service\":\"" $2 "\", \"protocol\":\"" $3 "\"],"}'
+    } | ../agents/ag_netd 3>&1 >/dev/null \
+	| sed "s/^/$pkg,&/" \
+	| awk -F, '{print "$[\"package\":\"" $1 "\", \"service\":\"" $2 "\", \"protocol\":\"" $3 "\", \"program\":\"" $4 "\"],"}'
 done
 echo "]"
-rm -f $DIR/*
+mv -f $DIR/* $DIR.bak
