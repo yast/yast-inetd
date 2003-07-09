75c95234dbcd700a3ffb21ddc43809648b60e255
Martin Vidner <mvidner@suse.cz>
2003-07-09 15:35:02 +0000

    New scripts for generating the complete config data.
    svn path=/trunk/inetd/; revision=10374

--- src/all-services.sh
+++ src/all-services.sh
@@ -2,43 +2,38 @@
 # $Id$
 # required: pdb-commandline.rpm
 #
-# Find all files in /etc/xinetd.d in the autobuild
-# and create a ycp file linking the declared services and the packages
-# Currently the format is:
-# A list of maps, having the keys "package", "service", "port", "program"
-# and string values
-# Program is the base name. It is the real program, not tcpd.
 
-SX=all-services
-:> $SX.ycp
-exec > $SX.ycp
+# generate default_conf_{inet,xinetd}.ycp, as required by
+# http://w3.suse.de/~kkaempf/yast2/planning/descr/improvement_24.html
+# by
+# - querying PDB for all config files
+# - extracting them all
+# - running the agent (for inetd, xinetd)
+# - for each program, quering the PDB which RPM it is in
+
+SX=default_conf
+
+# $1: package name
+# $2: file (wildcard) to extract
+function extract() {
+    echo "$1" "$2" >&2
+    # rpm3: etc/foo, rpm4: ./etc/foo
+    rpm2cpio /work/CDs/all/full-i386/suse/*/$1.rpm \
+	| cpio -idvm --no-absolute-filenames "./$2" "$2"
+}
+
+extract inetd  etc/inetd.conf
+extract xinetd etc/xinetd.conf
 
 # no leading slash
 DIR=etc/xinetd.d
 pdb query --filter "rpmdir:/$DIR" --attribs packname > $SX-pkgs
 
-echo "// A mapping between all available services and rpm packages"
-echo "// Author: Martin Vidner <mvidner@suse.cz>"
-echo "// \$Id\$"
-echo -n "// Generated on "
-LANG=C date
-echo "["
-sort $SX-pkgs | while read pkg; do
-    mv -f $DIR/* $DIR.bak
-    rpm2cpio /work/CDs/all/full-i386/suse/*/$pkg.rpm \
-	| cpio -idvm --no-absolute-filenames "$DIR/*"
-    # Use the agent to parse the config files
-    # It has a hack to print "$service,$port,$program" to fd3
-    {
-	echo "Xinetd(\"./$SX.conf\")"
-	# now it is necessary to Read to parse the files
-	echo "Read(.services)"
-	# services-by-package
-	echo "Execute(.sbp)"
-	echo "result(nil)"
-    } | ../agents/ag_netd 3>&1 >/dev/null \
-	| sed "s/^/$pkg,&/" \
-	| awk -F, '{print "$[\"package\":\"" $1 "\", \"service\":\"" $2 "\", \"protocol\":\"" $3 "\", \"program\":\"" $4 "\"],"}'
-done
-echo "]"
 mv -f $DIR/* $DIR.bak
+sort $SX-pkgs | while read pkg; do
+    extract $pkg "$DIR/*"
+done
+
+#proceed in YCP:
+/sbin/yast2 $SX_create.ycp
+
